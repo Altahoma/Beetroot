163a2c024e45920a9e430d916163ef0e7a9f275e
altahoma <altahoma@gmail.com>
2022-08-12 19:04:29 +0300

    Initial commit

---
+++ Lesson 2/task-2.py
@@ -0,0 +1,9 @@
+# 'Hellow Beetroot Academy!'
+print('Hellow Beetroot Academy!')
+
+# 'My_name_is_Eugene'
+print('My', 'name', 'is', 'Eugene', sep='_')
+
+# 'Today is 12-08-2022'
+print('Today is', end=' ')
+print('12', '08', '2022', sep='-')
