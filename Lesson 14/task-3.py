7864e871b4834dcd405e366086add5a18d9f3285
altahoma <altahoma@gmail.com>
2022-09-24 11:12:39 +0300

    Now wrapper() takes one positional argument

--- Lesson 14/task-3.py
+++ Lesson 14/task-3.py
@@ -1,18 +1,18 @@
 def arg_rules(type_: type, max_length: int, contains: list):
     def check_rules(func):
-        def wrapper(*args):
-            if type(*args) != type_:
-                print(f'Wrong type: {type(*args)} instead {type_} ')
+        def wrapper(arg):
+            if type(arg) != type_:
+                print(f'Wrong type: {type(arg)} instead {type_} ')
                 return False
-            elif len(*args) > max_length:
-                print(f'The length of \'{args[0]}\' is more than allowed: max={max_length}')
+            elif len(arg) > max_length:
+                print(f'The length of \'{arg}\' is more than allowed: max={max_length}')
                 return False
-            elif any(symbols not in args[0] for symbols in contains):
+            elif any(symbols not in arg for symbols in contains):
                 print(f'Do not contain needed symbols: {", ".join(contains)}')
                 return False
             else:
                 print('All checks passed')
-                return func(*args)
+                return func(arg)
         return wrapper
     return check_rules
 
