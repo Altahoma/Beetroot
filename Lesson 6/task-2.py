34021324081442758cd9f26dc26186a89fae0ea7
altahoma <altahoma@gmail.com>
2022-09-24 11:19:22 +0300

    mutual_nums contains only common nums

--- Lesson 6/task-2.py
+++ Lesson 6/task-2.py
@@ -10,5 +10,5 @@ while counter < 10:
     nums_2.append(random.randint(1, 10))
     counter += 1
 
-unique_nums = list(set(nums_1 + nums_2))
-print(unique_nums)
+mutual_nums = list(set(nums_1).intersection(set(nums_2)))
+print(mutual_nums)
